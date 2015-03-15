c11809b8319374d4c0802f406b2cc4a0bacf3c97
bjorgheidur <bjorgheidur12@ru.is>
2015-03-15 14:19:56 +0000

    removing conflict errors

--- client/controllers/LoginController.js
+++ client/controllers/LoginController.js
@@ -6,26 +6,20 @@ angular.module("schoolApp").controller("LoginController", ["$scope", "$location"
 			password: ""
 		};
 
-<<<<<<< Updated upstream
 		$scope.errors = [];
-=======
+
 		$scope.error = {
 			available: false,
 			empty: false
 		};
->>>>>>> Stashed changes
 
 		$scope.login = function () {
 
 			if ($scope.user.username && $scope.user.password) {
 				apiFactory.login($scope.user.username, $scope.user.password)
 					.then(function (results) {
-<<<<<<< Updated upstream
 
 						console.log("LoginController results", results);
-
-=======
->>>>>>> Stashed changes
 						if (results.status === 200) {
 
 							userFactory.setUser(results.data.User);
@@ -38,24 +32,16 @@ angular.module("schoolApp").controller("LoginController", ["$scope", "$location"
 						}
 						// Something wrong with credentials most likely - add error for that
 						else {
-
-<<<<<<< Updated upstream
-=======
 							$scope.error.available = true;
 							console.log("error available = true");
-
->>>>>>> Stashed changes
 						}
 
 					});
 			}
-<<<<<<< Updated upstream
-=======
 			// Username and password must be filled in to attempt to login
 			else {
 				$scope.error.available = true;
 			}
->>>>>>> Stashed changes
 
 		};
 
